4170ec494c5b9dfc03f579a19e03bb4d7d5f3c42
viiftw <datdn.it@gmail.com>
2019-03-06 11:10:41 +0700

    vue styling

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="skills">
+  <div class="container">
     <div class="holder">
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{index}}. {{data.skill}}</li>
+        <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
       </ul>
-      <p v-if="skills.length >= 1">You have more than 1 skill</p>
-      <p v-else>You have less than or equal to 1 skill</p>
+      <p>These are the skills that you possess.</p>
     </div>
   </div>
 </template>
@@ -18,7 +17,12 @@ export default {
       skills: [
           { "skill": "Vue.js" },
           { "skill": "Frontend Developer" }
-      ]
+      ],
+      alertObject: {
+        bgColor: 'yellow',
+        bgWidth: '100%',
+        bgHeight: '30px'
+      }
     }
   }
 }
@@ -26,18 +30,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
 </style>
