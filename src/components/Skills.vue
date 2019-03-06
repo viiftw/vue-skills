f3fc3f135984a48f13555bdcd78ac70b2d7ae27e
viiftw <datdn.it@gmail.com>
2019-03-06 11:32:05 +0700

    add animation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,12 +3,20 @@
     
     <form @submit.prevent="addSkill">
       <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+
       <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+
+      <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+      </transition>
+
     </form>
 
     <div class="holder">
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+        </transition-group>
       </ul>
       <p>These are the skills that you possess.</p>
     </div>
@@ -44,6 +52,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
   .holder {
     background: #fff;
   }
@@ -87,5 +96,23 @@ export default {
     display: inline-block;
     padding: 5px;
     margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+  animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 </style>
