f385c5091c521d03c23dc2454ec71d3244216323
viiftw <datdn.it@gmail.com>
2019-03-06 10:57:01 +0700

    vue templating

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="skills">
-    Skills
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{index}}. {{data.skill}}</li>
+      </ul>
+      <p v-if="skills.length >= 1">You have more than 1 skill</p>
+      <p v-else>You have less than or equal to 1 skill</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
-    msg: String
+  data() {
+    return {
+      skills: [
+          { "skill": "Vue.js" },
+          { "skill": "Frontend Developer" }
+      ]
+    }
   }
 }
 </script>
